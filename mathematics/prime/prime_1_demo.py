bd4124b9d89bd639d327e5734d943b73d01dd28b
eric_sunah@126.com <sunaihua@126>
2017-11-06 18:03:24 +0800

    update

--- mathematics/prime/prime_1_demo.py
+++ mathematics/prime/prime_1_demo.py
@@ -7,11 +7,20 @@ from utils.ptime import fn_time
 '''
 
 
+def iter_list(num):
+    # half_num = num / 2
+    return range(2, num)
+
+
 def is_prime(num):
     if num < 2:
         return False
-    half_num = num / 2
-    for n in range(2, half_num):
+    if num == 2:
+        return True
+    iter_nums = iter_list(num)
+    if not iter_nums:
+        return False
+    for n in iter_nums:
         # print '%s/%s=%s' % (str(num), str(n), str(num % n))
         if num % n == 0:
             return False
@@ -22,8 +31,12 @@ def prime_list(max):
     for num in range(max):
         if is_prime(num):
             yield num
+
+
 @fn_time
 def main():
-    print len(list(prime_list(100000)))
+    # print len(list(prime_list(1000)))
+    print list(prime_list(100000))
+
 
 main()
